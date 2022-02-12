840565216fe13bc04b6683e489b0e7f187e6c8a6
CarloLim01 <carlolim543@gmail.com>
2022-02-11 17:34:47 -0800

    Resume (Importing Name, FPDF and json)

---
+++ Resume.py
@@ -0,0 +1,16 @@
+#PDF Resume Creator
+	#Create a python program that will create your personal resume in PDF format
+	#All personal details are stored in a JSON file
+	#Your program should read the JSON file and write the details in the PDF
+	#The output file should be: LASTNAME_FIRSTNAME.pdf
+
+#Note:
+	#Search for available PDF library that you can use
+	#Search how data is structured using JSON format
+	#Search how to read JSON file
+	#You will create the JSON file manually
+	#Your code should be in github before Feb12
+
+from tokenize import Name
+from fpdf import FPDF
+import json
